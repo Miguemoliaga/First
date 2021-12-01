fd41498892f02931f21269af2878eb6e2648a406
miguelmoliaga <miguel.mo1999@gmail.com>
2021-12-01 22:24:27 +0100

    login functions

--- app/src/main/java/com/example/firstmiguelmartinezoliaga/LoginActivity.java
+++ app/src/main/java/com/example/firstmiguelmartinezoliaga/LoginActivity.java
@@ -1,6 +1,62 @@
 package com.example.firstmiguelmartinezoliaga;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    Button entrar;
+    Button volver;
+    TextView signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        entrar = (Button)findViewById(R.id.button);
+        volver = (Button)findViewById(R.id.buttoninverso);
+        signup = (TextView)findViewById(R.id.textoSignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignup(view);
+            }
+        });
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain(view);
+            }
+        });
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void openMain(View v) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void openSignup(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+
 }
