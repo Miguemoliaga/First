5d4eced683d593a70550dfe851a2a696e9eb6406
miguelmoliaga <miguel.mo1999@gmail.com>
2021-12-01 22:25:11 +0100

    login functions flags

--- app/src/main/java/com/example/firstmiguelmartinezoliaga/LoginActivity.java
+++ app/src/main/java/com/example/firstmiguelmartinezoliaga/LoginActivity.java
@@ -56,6 +56,8 @@ public class LoginActivity extends AppCompatActivity {
 
     public void openSignup(View v) {
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
