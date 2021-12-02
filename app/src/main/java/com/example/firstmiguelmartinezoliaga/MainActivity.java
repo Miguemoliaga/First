8ce01740cc4596fa7268f6e3247592161c7933e2
miguemoliaga <miguel.mo1999@gmail.com>
2021-12-02 13:24:27 +0100

    webview funct

--- app/src/main/java/com/example/firstmiguelmartinezoliaga/MainActivity.java
+++ app/src/main/java/com/example/firstmiguelmartinezoliaga/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.firstmiguelmartinezoliaga;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private SwipeRefreshLayout swipeLayout;
+    private WebView miVisorWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView mycontext = (WebView) findViewById(R.id.vistaweb);
+        registerForContextMenu(mycontext);
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+        miVisorWeb = (WebView) findViewById(R.id.vistaweb);
+        miVisorWeb.getSettings().setBuiltInZoomControls(true);
+        miVisorWeb.loadUrl("https://thispersondoesnotexist.com/");
     }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(MainActivity.this, "Imagen actualizada", Toast.LENGTH_LONG);
+            toast0.show();
+            miVisorWeb.reload();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
 }
